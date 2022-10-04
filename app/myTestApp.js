fd0f927fb4b368adad69cc0b98facebd4b8aef28
EndymionSpr <endymion1@vp.pl>
2022-10-04 10:10:33 +0200

    Added ability to edit some parts of chart in view1

--- app/myTestApp.js
+++ app/myTestApp.js
@@ -1,34 +1,35 @@
-// import * as localStore from '@zellwk/javascript/browser/localstore'
 
 var myTestApp = angular.module('myTestApp',['LocalStorageModule']);
 
-ourStorage = window.localStorage;
 
-// ourStorage = localStore;
 
 myTestApp.controller('myController', function($scope, localStorageService){
     $scope.myMessage=["Jacob","Peter"];
-    // ourStorage = window.localStorage;
     
     $scope.submit = function(){
         $scope.myMessage.push($scope.firstName);
-    }
+    };
     $scope.delete = function(){
         $scope.myMessage.splice($scope.myMessage.indexOf($scope.firstName),1);
-    }
+    };
     $scope.update = function(){
         $scope.myMessage[$scope.myMessage.indexOf($scope.firstName)]=$scope.updateName;
-    }
+    };
     $scope.save = function(){
-        // Content of fucntion that saves to DB.
-        // ourStorage.setItem($scope.saveAs, JSON.stringify($scope.myMessage));
+        // Content of function that saves to DB.
         localStorageService.set($scope.saveAs, ($scope.myMessage));
         
-    }
+    };
     $scope.load = function(){
-        // $scope.myMessage = JSON.parse(ourStorage.getItem($scope.loadThis));
+        // Content of function that loads from DB.
         $scope.myMessage = (localStorageService.get($scope.loadThis));
         
-    }
+    };
     
+    // View1 part with Form for chart manipulation
+
+    $scope.firstChartVar = "120";
+    $scope.chartType = "donut";
+    
+
 });
